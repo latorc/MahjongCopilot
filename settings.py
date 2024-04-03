bcd467b64766e5985045e3ae18e6d1452855ce93
latorc <65785354+latorc@users.noreply.github.com>
2024-04-03 16:44:49 +0800

    settings read utf-8

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ class Settings:
     def load_json(self) -> dict:
         """ Load settings from json file into dict"""
         if pathlib.Path(self._json_file).exists():
-            with open(self._json_file, 'r') as file:
+            with open(self._json_file, 'r',encoding='utf-8') as file:
                 settings_dict:dict = json.load(file)
         else:
             settings_dict = {}
