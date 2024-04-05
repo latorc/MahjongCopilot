affb9ad7382500952c76f5be574da9cd8ae8323d
latorc <65785354+latorc@users.noreply.github.com>
2024-04-05 12:40:30 +0800

    test version

---
+++ updater.py
@@ -0,0 +1,20 @@
+""" check for update"""
+
+
+
+class Updater:
+    def __init__(self):
+        self.url = "https://maco.clandoom.com"
+
+    def check_update(self):
+        """ check for update, and return new version number if any. return None if old"""
+        # read url/version file content into str
+        # compare with current version
+        # return new version number if any. return None if old
+        
+
+    def update(self):
+        pass
+
+    def update_available(self):
+        pass
