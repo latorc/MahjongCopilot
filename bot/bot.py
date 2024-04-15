fff6b485ee9d199f76f44b0bd8f72b30b506d12b
latorc <65785354+latorc@users.noreply.github.com>
2024-04-15 17:05:49 +0800

    feat: support Akagi 3p model

--- bot/bot.py
+++ bot/bot.py
@@ -5,39 +5,46 @@ from enum import Enum
 from abc import ABC, abstractmethod
 
 import common.mj_helper as mj_helper
-
+from common.utils import GameMode
 
 class BotType(Enum):
     """ Model type for bot"""
     LOCAL = "Local"
     MJAPI = "MJAPI"
 
-
-def reaction_convert_meta(reaction:dict):
+def reaction_convert_meta(reaction:dict, is_3p:bool=False):
     """ add meta_options to reaction """
     if 'meta' in reaction:
         meta = reaction['meta']
-        reaction['meta_options'] = mj_helper.meta_to_options(meta)
+        reaction['meta_options'] = mj_helper.meta_to_options(meta, is_3p)
 
 class Bot(ABC):
     """ Bot Interface class
     bot follows mjai protocol
     ref: https://mjai.app/docs/highlevel-api
-    Note: reach msg is implemented differently. 
-    Reach msg has reach_dahai attached, which is a 'dahai' msg, indicating the dahai action after reach
-    msgs have 'meta_options', which is a translation of 'meta' into list of (mjai tile, weight)"""
+    Note: Reach msg has additional 'reach_dahai' key attached,
+    which is a 'dahai' msg, representing the subsequent dahai action after reach
+    """
 
     def __init__(self, bot_type:BotType, name:str="Bot") -> None:
         self.type = bot_type
         self.name = name
         self._initialized:bool = False
         self.seat:int = None
+    
+    @property
+    def supported_modes(self) -> list[GameMode]:
+        """ return suported game modes"""
+        return [GameMode.MJ4P]
 
-    def init_bot(self, seat:int,is_3p:bool=False):
-        """ Initialize the bot before the game starts. Bot must be initialized before a new game""" 
-        self.is_3p = is_3p
+    def init_bot(self, seat:int, mode:GameMode=GameMode.MJ4P):
+        """ Initialize the bot before the game starts. Bot must be initialized before a new game
+        params:
+            seat(int): Player seat index
+            mode(GameMode): Game mode"""
+         
         self.seat = seat
-        self._init_bot_impl()
+        self._init_bot_impl(mode)
         self._initialized = True
 
     @property
@@ -46,7 +53,7 @@ class Bot(ABC):
         return self._initialized
        
     @abstractmethod
-    def _init_bot_impl(self):
+    def _init_bot_impl(self, mode:GameMode=GameMode.MJ4P):
         """ Initialize the bot before the game starts."""
 
     @abstractmethod
