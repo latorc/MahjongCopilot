3b0450a8d170ba3637be14424002731015f59c12
latorc <65785354+latorc@users.noreply.github.com>
2024-04-15 18:14:14 +0800

    bump 0.5.0

--- scripts/generate_exe.bat
+++ scripts/generate_exe.bat
@@ -3,7 +3,7 @@ REM Windows
 set PLAYWRIGHT_BROWSERS_PATH=0
 playwright install chromium
 
-REM rmdir /s /q dist
+rmdir /s /q dist\MahjongCopilot
 pyinstaller --windowed --noconfirm --name=MahjongCopilot --icon=resources/icon.ico main.py 
 if errorlevel 1 (
     echo PyInstaller encountered an error.
@@ -14,5 +14,6 @@ REM robocopy . .\dist\MahjongCopilot settings.json
 REM robocopy models dist\MahjongCopilot\models /E
 robocopy resources dist\MahjongCopilot\resources /E
 robocopy liqi_proto dist\MahjongCopilot\liqi_proto /E
+robocopy .\libriichi3p\ dist\MahjongCopilot\_internal\libriichi3p\ *.pyd *.so /E
 robocopy .venv\Lib\site-packages\playwright\driver\package\.local-browsers dist\MahjongCopilot\_internal\playwright\driver\package\.local-browsers /E
 explorer.exe dist
