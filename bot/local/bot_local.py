91f535b988eafb6d8b6039c1ec78a39714792f4a
Calvin Div <diverpet@icloud.com>
2024-05-27 15:04:52 +0800

    用分离进程解决 rust panick导致崩溃的问题

--- bot/local/bot_local.py
+++ bot/local/bot_local.py
@@ -8,6 +8,39 @@ from common.log_helper import LOGGER
 from bot.local.engine import get_engine
 from bot.bot import BotMjai, GameMode
 from common.settings import Settings
+import multiprocessing
+
+# 尝试获取mjai.bot实例，该方法可能会导致 panick，需要在分离进程中使用
+def create_bot_instance(queue):
+    import riichi3p
+    try:
+        # 尝试创建一个mjai.bot实例
+        riichi3p.online.Bot(1)
+        queue.put(True)  # 将成功的标志放入队列
+    except Exception as e:
+        LOGGER.warning("Cannot create bot: %s", e, exc_info=True)
+        LOGGER.warning("Could be missing model.pth file in path ./mjai/bot_3p")
+        queue.put(False)  # 将失败的标志放入队列
+
+# 使用分离进程尝试创建bot实例
+def try_create_ot2_bot():
+    queue = multiprocessing.Queue()
+    process = multiprocessing.Process(target=create_bot_instance, args=(queue,))
+    process.start()
+
+    # 尝试从队列中获取结果，设置超时时间防止无限等待
+    try:
+        success = queue.get(timeout=10)  # 设置适当的超时时间，例如10秒
+    except Exception as e:
+        LOGGER.error("Failed to retrieve the result from the subprocess: %s", e)
+        success = False
+
+    process.join()
+
+    if not success or process.exitcode != 0:
+        LOGGER.error("Failed to create bot or detected a crash in the subprocess with exit code %s", process.exitcode)
+        return False
+    return True
 
 
 class BotMortalLocal(BotMjai):
@@ -35,9 +68,12 @@ class BotMortalLocal(BotMjai):
                     if settings.enable_ot2_for_3p:
                         import riichi3p
                         try :
-                            # 尝试创建一个mjai.bot实例
-                            riichi3p.online.Bot(1)
-                            self._engines[k] = "./mjai/bot_3p/model.pth"
+                            # 用分离进程尝试创建一个mjai.bot实例
+                            if try_create_ot2_bot():
+                                self._engines[k] = "./mjai/bot_3p/model.pth"
+                            else:
+                                LOGGER.warning("Cannot create bot for OT2 model %s.", k, exc_info=True)
+                                LOGGER.warning("Could be missing model.pth file in path ./mjai/bot_3p")
                         except Exception as e:
                             LOGGER.warning("Cannot create bot for OT2 model %s: %s", k, e, exc_info=True)
                             LOGGER.warning("Could be missing model.pth file in path ./mjai/bot_3p")
