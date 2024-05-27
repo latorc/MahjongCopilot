51be7b8fc343cbf0e065e9f89af72e2b32dc31fb
Calvin Div <diverpet@icloud.com>
2024-05-27 14:20:19 +0800

    配置 OT2 模型时检测可用性

--- bot/factory.py
+++ bot/factory.py
@@ -8,17 +8,24 @@ from .akagiot.bot_akagiot import BotAkagiOt
 
 
 MODEL_TYPE_STRINGS = ["Local", "AkagiOT", "MJAPI"]
+OT2_MODEL_PATH = "./mjai/bot_3p/model.pth"
 
 
 def get_bot(settings:Settings) -> Bot:
     """ create the Bot instance based on settings"""
     
     match settings.model_type:
-        case "Local":   
-            model_files:dict = {
-                GameMode.MJ4P: sub_file(Folder.MODEL, settings.model_file),
-                GameMode.MJ3P: sub_file(Folder.MODEL, settings.model_file_3p)
-            }
+        case "Local":
+            if settings.enable_ot2_for_3p:
+                model_files:dict = {
+                    GameMode.MJ4P: sub_file(Folder.MODEL, settings.model_file),
+                    GameMode.MJ3P: OT2_MODEL_PATH
+                }
+            else:
+                model_files:dict = {
+                    GameMode.MJ4P: sub_file(Folder.MODEL, settings.model_file),
+                    GameMode.MJ3P: sub_file(Folder.MODEL, settings.model_file_3p)
+                }
             bot = BotMortalLocal(model_files)
         case "AkagiOT":
             bot = BotAkagiOt(settings.akagi_ot_url, settings.akagi_ot_apikey)
